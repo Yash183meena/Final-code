4305c3ca7456944256910bcefa77a628f3ac1e09
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2024-11-22 20:30:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // Code here
+        int n=arr.size(),great=INT_MIN;
+        vector<int>greater(n),leaders;
+        
+        greater[n-1]=-1;
+        
+        for(int i=n-1;i>=0;i--){
+            if(great <= arr[i]){
+                greater[i]=-1;
+                great=arr[i];
+            }
+            else{
+                greater[i]=great;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(greater[i]==-1){
+                leaders.push_back(arr[i]);
+            }
+        }
+        
+        return leaders;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.leaders(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no leaders are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
