e24db31dcb5d63a05bf776cc3ee65b1d9464b5b8
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-01-15 18:23:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the next greater element for each element of the array.
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        stack<int>stk;
+        vector<int>greater(n);
+        for(int i=arr.size()-1;i>=0;i--){
+            
+            while(!stk.empty() && stk.top() <= arr[i] ){
+                stk.pop();
+            }
+            
+            if(!stk.empty()){
+                greater[i]=stk.top();;
+            }
+            
+            else{
+                greater[i]=-1;
+            }
+            
+            stk.push(arr[i]);
+        }
+        
+        return greater;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after reading t
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Reading the entire input line for the array
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.nextLargerElement(a);
+
+        // Print the result in the required format
+        for (int i = 0; i < result.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << result[i];
+        }
+        cout << endl;        // Ensure new line after each test case output
+        cout << "~" << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
