d3d6b2c94a6080debc896fa0ac6bc7a3efb1a901
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-01-20 19:05:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
@@ -10,49 +10,52 @@ using namespace std;
 class Solution {
   public:
   
-    void dfs(vector<vector<int>>& grid,int row,int col,vector<vector<bool>>&visited,
-    vector<pair<int,int>>&island,int base_row,int base_col,int n,int m){
-        
+    void dfs(int row,int col,vector<vector<int>>& grid,int ref_x,int ref_y,
+       vector<vector<bool>>&visited,vector<pair<int,int>>&vec,int n,int m){
         
         visited[row][col]=true;
+        vec.push_back({ref_x-row,ref_y-col});
         
-        int delrow[]={1,-1,0,0};
-        int delcol[]={0,0,1,-1};
-        
-        island.push_back({row-base_row,col-base_col});
+        int delrow[]={0,0,-1,1};
+        int delcol[]={1,-1,0,0};
         
         for(int i=0;i<4;i++){
             
-            int newrow=row+delrow[i];
-            int newcol=col+delcol[i];
+            int newrow = row + delrow[i];
+            int newcol = col + delcol[i];
             
-            if(newcol<m && newrow<n && newrow>=0 && newcol>=0 && !visited[newrow][newcol] 
-            && grid[newrow][newcol]==1){
-                dfs(grid,newrow,newcol,visited,island,base_row,base_col,n,m);
+            if(newrow<n && newcol<m && newrow>=0 && newcol>=0 && grid[newrow][newcol]==1 
+            && !visited[newrow][newcol]){
+                
+                dfs(newrow,newcol,grid,ref_x,ref_y,visited,vec,n,m);
             }
         }
+        
     }
     
     int countDistinctIslands(vector<vector<int>>& grid) {
         // code here
-        int n=grid.size(),m=grid[0].size();
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
         vector<vector<bool>>visited(n,vector<bool>(m,false));
-        vector<pair<int,int>>island;
         set<vector<pair<int,int>>>st;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    dfs(grid,i,j,visited,island,i,j,n,m);
-                    st.insert(island);
-                    island.clear();
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                
+                vector<pair<int,int>>vec;
+                if( grid[i][j]==1 && !visited[i][j]){
+                    
+                    dfs(i,j,grid,i,j,visited,vec,n,m);
+                    st.insert(vec);
                 }
             }
         }
         
-        int distint=st.size();
-        return distint;
-
+        return st.size();
+        
     }
 };
 
