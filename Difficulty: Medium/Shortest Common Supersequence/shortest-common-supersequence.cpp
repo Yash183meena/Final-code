6a797322b9c136ff6970afe9500bdbdbbe2daaae
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-01-22 11:58:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -13,39 +13,37 @@ class Solution {
     // Function to find length of shortest common supersequence of two strings.
     int solve(string &s1, string &s2,int i,int j,vector<vector<int>>&dp){
         
-        if(j==s2.size()){
-            return s1.size()-i;
-        }
-        
-        if(i==s1.size()){
+        if(i >=s1.size()){
             return s2.size()-j;
+        }    
+        
+        if(j >= s2.size()){
+            return s1.size()-i;
         }
         
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         
-        if(s1[i]==s2[j]){
+        if(s1[i] == s2[j]){
             return dp[i][j] = 1 + solve(s1,s2,i+1,j+1,dp);
         }
         
-        //if character not matches
-        int Take_s1_character = 1 + solve(s1,s2,i+1,j,dp);
+        int taking_the_s1 = 1 + solve(s1,s2,i+1,j,dp);
         
-        int Take_s2_character = 1 + solve(s1,s2,i,j+1,dp);
+        int taking_the_s2 = 1 + solve(s1,s2,i,j+1,dp);
         
-        return dp[i][j] = min(Take_s1_character,Take_s2_character);
-        
+        return dp[i][j] = min(taking_the_s1,taking_the_s2);
     }
     
     int shortestCommonSupersequence(string &s1, string &s2) {
         // code here
-        int n=s1.size(),m=s2.size();
+        
+        int n=s1.size();
+        int m=s2.size();
         
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        
         return solve(s1,s2,0,0,dp);
-        
     }
 };
 
