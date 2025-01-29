8fc5f0bf89c1344404bf7d6eb07e63608afa724d
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-01-30 03:05:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -9,26 +9,29 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    void topoSort(int node,map<int,vector<pair<int,int>>>&graph,stack<int>&topo, vector<int>&visited){
-        
-        visited[node]=true;
-        
-        for(auto newnode_pair : graph[node]){
-            
-            int newnode=newnode_pair.first;
-            
-            if(!visited[newnode]){
-                topoSort(newnode,graph,topo,visited);
-            }
-        }
-        
-        topo.push(node);
-    }
-    
+   void dfs(int node,map<int,vector<pair<int,int>>>&graph,vector<int>&visited,
+   stack<int>&stk){
+       
+       visited[node]=true;
+       
+       for(auto newnode_pair : graph[node]){
+           
+           int newnode = newnode_pair.first;
+           
+           if(!visited[newnode]){
+               dfs(newnode,graph,visited,stk);
+           }
+       }
+       
+       stk.push(node);
+   }
+   
     vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
         // code here
-    
         map<int,vector<pair<int,int>>>graph;
+        vector<int>distance(V,INT_MAX);
+        stack<int>stk;
+        vector<int>visited(V,false);
         
         for(auto edge : edges){
             int from = edge[0];
@@ -38,30 +41,27 @@ class Solution {
             graph[from].push_back({to,weight});
         }
         
-        vector<int>visited(V,false);
-        vector<int>distance(V,INT_MAX);
-        stack<int>topo;
-        
-        //if there are more than one componenets
+        //loop beacuse if there is more than one componenet in graph
         for(int i=0;i<V;i++){
             if(!visited[i]){
-                topoSort(0,graph,topo,visited);
+                dfs(0,graph,visited,stk);
             }
         }
         
         distance[0]=0;
         
-        while(!topo.empty()){
+        while(!stk.empty()){
             
-            int node = topo.top();
-            topo.pop();
+            int node = stk.top();
+            stk.pop();
             
             for(auto newnode_pair : graph[node]){
                 
                 int newnode = newnode_pair.first;
                 int weight = newnode_pair.second;
                 
-                if(distance[newnode] > distance[node] + weight){
+                if( distance[newnode] > distance[node] + weight ){
+                    
                     distance[newnode] = distance[node] + weight;
                 }
             }
