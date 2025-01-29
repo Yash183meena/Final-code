8dc43cef3f0cc70e0db4cb0245b5492f21e2cfcd
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-01-29 15:39:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -7,44 +7,37 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool isCyclic(int node,vector<vector<int>>& adj,map<int,int>&parent,vector<int>&visited){
-        
-        queue<int>que;
-        que.push(node);
+    bool isCyclic(int node,vector<vector<int>>& adj,vector<bool>&visited,int parent){
         
         visited[node]=true;
         
-        while(!que.empty()){
+        for(auto newnode : adj[node]){
             
-            int node = que.front();
-            que.pop();
+            if(newnode == parent){
+                continue;
+            }
             
-            for(auto newnode : adj[node]){
-                
-                if(!visited[newnode]){
-                    parent[newnode]=node;
-                    que.push(newnode);
-                    visited[newnode]=true;
-                }
-                
-                else if(parent[node]!=newnode){
-                    return true;
-                }
+            if(!visited[newnode]){
+                isCyclic(newnode,adj,visited,node);
+            }
+            
+            else{
+                return true;
             }
         }
+        
+        visited[node]=false;
         
         return false;
     }
     
     bool isCycle(vector<vector<int>>& adj) {
         // Code here
-        
         int v=adj.size();
-        map<int,int>parent;
-        vector<int>visited(v,false);
+        vector<bool>visited(v,false);
         
         for(int i=0;i<v;i++){
-            if(!visited[i] && isCyclic(i,adj,parent,visited)){
+            if(isCyclic(i,adj,visited,-1)){
                 return true;
             }
         }
