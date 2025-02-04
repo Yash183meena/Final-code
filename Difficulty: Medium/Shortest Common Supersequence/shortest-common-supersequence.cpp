18c40ea6065a005c10baacff3ab10a006818f007
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-02-04 23:48:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -11,33 +11,33 @@ using namespace std;
 class Solution {
   public:
     // Function to find length of shortest common supersequence of two strings.
-    int solve(int i,int j,string s1,string s2,vector<vector<int>>&dp){
-        
-        if(i>=s1.size()){
-            return s2.size()-j;
-        }
-        
-        if(j>=s2.size()){
-            return s1.size() -i;
-        }
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        
-        if(s1[i] == s2[j]){
-            return dp[i][j] = 1 + solve(i+1,j+1,s1,s2,dp);
-        }
-        
-        return dp[i][j] = 1 + min(solve(i+1,j,s1,s2,dp),solve(i,j+1,s1,s2,dp));
-        
-    }
-    
     int shortestCommonSupersequence(string &s1, string &s2) {
         // code here
-        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size(),-1));
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         
-        return  solve(0,0,s1,s2,dp);
+        for(int i=n;i>=0;i--){
+            for(int j=m;j>=0;j--){
+                
+                if(i==n){
+                    dp[i][j] = m-j;
+                }
+                
+                else if(j==m){
+                    dp[i][j] = n-i;
+                }
+                
+                else if( s1[i] == s2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                
+                else{
+                    dp[i][j] = 1 + min(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        
+        return dp[0][0];
     }
 };
 
