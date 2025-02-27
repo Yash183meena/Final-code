b2530c3c3f6bdfa2f85e5a0c3062ded289487531
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-02-27 13:35:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        
+        Node*head=NULL,*temp;
+        
+        for(int i=0;i<arr.size();i++){
+            
+            if(!head){
+                head=new Node(arr[i]);
+                temp=head;
+            }
+            
+            else{
+                Node*newnode=new Node(arr[i]);
+                temp->next=newnode;
+                temp=temp->next;
+            }
+        }
+        
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        Node* ans = ob.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
