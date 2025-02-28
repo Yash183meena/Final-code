b1f757ff6c6459b68a3a901c018a15cdd67acbba
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-02-28 10:23:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-  
+    
     void merge(vector<int>& arr, int l, int mid, int r){
         
-        vector<int>left;
-        vector<int>right;
+        int n1 = mid-l+1;
+        int n2 = r-mid;
         
-        for(int i=l;i<=mid;i++){
+        vector<int>left,right;
+        
+        for(int i=l;i<l+n1;i++){
             left.push_back(arr[i]);
         }
         
@@ -20,55 +23,53 @@ class Solution {
             right.push_back(arr[i]);
         }
         
-        int i=0,j=0;
+        int i=0,j=0,k=l;
         
-        int k=l;
-        
-        while(i<left.size() && j<right.size()){
+        while( i<n1 && j<n2){
             
-            if(left[i] < right[j]){
+            if( left[i] <= right[j]){
                 arr[k] = left[i];
-                k++;
                 i++;
+                k++;
             }
             
             else{
-                arr[k]=right[j];
-                k++;
+                arr[k] = right[j];
                 j++;
+                k++;
             }
         }
         
-        if(i==left.size()){
-            while(j<right.size()){
-                arr[k]=right[j];
-                k++;
-                j++;
-            }
+        while(i<n1){
+            arr[k] = left[i];
+            i++;
+            k++;
         }
         
-        else if(j==right.size()){
-            while(i<left.size()){
-                arr[k]=left[i];
-                k++;
-                i++;
-            }
+        while(j<n2){
+            arr[k] = right[j];
+            k++;
+            j++;
         }
+        
+        
     }
-   
+    
     void mergeSort(vector<int>& arr, int l, int r) {
         // code here
         
-        int mid=l + (r-l)/2;
-        
-        if(l<r){
+        if(l < r){
+            
+            int mid = l + (r - l)/2;
             
             mergeSort(arr,l,mid);
             mergeSort(arr,mid+1,r);
+            
             merge(arr,l,mid,r);
         }
     }
 };
+
 
 //{ Driver Code Starts.
 
