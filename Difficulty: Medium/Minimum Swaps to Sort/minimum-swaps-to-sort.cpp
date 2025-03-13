d7359252f94e8cec2fd9e33229af025831980941
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-03-13 10:57:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Swaps to Sort/minimum-swaps-to-sort.cpp
+++ Difficulty: Medium/Minimum Swaps to Sort/minimum-swaps-to-sort.cpp
@@ -6,35 +6,38 @@ using namespace std;
 // } Driver Code Ends
 
 
+
 class Solution {
   public:
     // Function to find the minimum number of swaps required to sort the array.
     int minSwaps(vector<int>& arr) {
         // Code here
         
-        vector<pair<int,int>>vec;
         int swaps=0;
+        vector<pair<int,int>>vec;
         
+        //step1:-->pushing all elements with index in the vector
         for(int i=0;i<arr.size();i++){
             vec.push_back({arr[i],i});
         }
         
+        //step2:-->sort that vector acc to elements
         sort(vec.begin(),vec.end());
         
-        for(int i=0;i<vec.size();i++){
+        //step3:--We can be track which elements is swapped out after the sorting
+        for( int i=0;i<vec.size();i++){
             
-            if(vec[i].second!=i){
+            if( i!=vec[i].second ){
+                swaps++;
                 swap(vec[i],vec[vec[i].second]);
                 i--;
-                swaps++;
             }
         }
-        
-        //cout<<swaps<<endl;
         
         return swaps;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
