390b8cf0a4fd3cecdc66ce17cf72be2a0538f8a5
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-03-13 17:58:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -4,50 +4,55 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     int solve(int i,int j,vector<vector<int>>& arr,vector<vector<int>>&dp){
         
-        if(i == arr.size()-1){
-            
-            return arr[i][j];
+        if(i>=arr.size() || j>=arr[0].size()){
+            return 0;
         }
         
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         
-        int a=INT_MIN,b=INT_MIN,c=INT_MIN;
+        int r=0,l=0,f=0;
         
-        if(j!=0){
-            a = arr[i][j] + solve(i+1,0,arr,dp);
+        //for running 
+        if(j==0){
+            r = arr[i][0] + max(solve(i+1,1,arr,dp),solve(i+1,2,arr,dp));
         }
         
-        if(j!=1){
-            b = arr[i][j] + solve(i+1,1,arr,dp);
+        //for fighting
+        else if(j==1){
+            f = arr[i][1] + max(solve(i+1,0,arr,dp),solve(i+1,2,arr,dp));
         }
         
-        if(j!=2){
-            c = arr[i][j] + solve(i+1,2,arr,dp);
+        //for learning
+        else if(j==2){
+            l = arr[i][2] + max(solve(i+1,0,arr,dp),solve(i+1,1,arr,dp));
         }
         
-        return dp[i][j] = max(a,max(b,c));
-        
+        return dp[i][j]=max({r,f,l});
     }
     
     int maximumPoints(vector<vector<int>>& arr) {
         // Code here
+        
         int n=arr.size();
         vector<vector<int>>dp(n+1,vector<int>(4,-1));
         int ans=INT_MIN;
         
-        for(int i=0;i<=2;i++){
-            ans=max(ans,solve(0,i,arr,dp));
+        for(int j=0;j<3;j++){
+            ans=max(ans,solve(0,j,arr,dp));
         }
         
         return ans;
+        
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
