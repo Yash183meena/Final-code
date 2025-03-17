8d0c337f2d86d58aa646e3e91e427c51c9c7b813
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-03-17 15:21:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -4,20 +4,24 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-  
     int longestCommonSubstr(string& s1, string& s2) {
         // your code here
-        int n=s1.size(),m=s2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        int n=s1.size();
+        int m=s2.size();
+        
         int ans=0;
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                if( s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                     ans=max(ans,dp[i][j]);
                 }
                 
@@ -30,6 +34,7 @@ class Solution {
         return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 
