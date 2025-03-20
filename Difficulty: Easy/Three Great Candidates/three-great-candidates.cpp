f51d10bfe7fb6e9bd8decc222159d44124e91df7
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-03-20 16:52:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Three Great Candidates/three-great-candidates.cpp
+++ Difficulty: Easy/Three Great Candidates/three-great-candidates.cpp
@@ -12,40 +12,43 @@ class Solution {
   public:
     int maxProduct(vector<int> arr) {
         // code here
-        int a=INT_MIN,b=INT_MIN,c=INT_MIN;
         
-        int d=INT_MAX,e=INT_MAX;
+        //three number with maximum product
+        
+        //done by maintaing the 5 variables
+        
+        int max1=INT_MIN,max2=INT_MIN,max3=INT_MIN;
+        int min1=INT_MAX,min2=INT_MAX;
         
         for(int i=0;i<arr.size();i++){
             
-            if( arr[i] > a ){
-                c=b;
-                b=a;
-                a=arr[i];
+            if(arr[i] > max1){
+                max3=max2;
+                max2=max1;
+                
+                max1=arr[i];
+            }
+            else if(arr[i] > max2){
+                max3=max2;
+                max2=arr[i];
+            }
+            else if(arr[i]>max3){
+                max3=arr[i];
             }
             
-            else if( arr[i] > b ){
-                c=b;
-                b=arr[i];
+            if(arr[i] < min1){
+                min2=min1;
+                min1=arr[i];
             }
-            
-            else if( arr[i] > c ){
-                c=arr[i];
+            else if(arr[i] < min2){
+                min2=arr[i];
             }
-            
-            if( arr[i] < d ){
-                e=d;
-                d=arr[i];
-            }
-            
-            else if( arr[i] <e ){
-                e=arr[i];
-            }
+    
         }
         
-        
-        return max(a*b*c,a*d*e);
+        return max(max1*min1*min2,max1*max2*max3);
     }
+    
 };
 
 
