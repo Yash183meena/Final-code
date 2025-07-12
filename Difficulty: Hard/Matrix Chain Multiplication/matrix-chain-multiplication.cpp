1f47bcd65513ee5fa7b73feaa25f3c357dfb9165
Yash183meena <151773531+Yash183meena@users.noreply.github.com>
2025-07-12 17:25:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -1,20 +1,10 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
-
 class Solution {
   public:
   
-    int solve(int i,int j,vector<int> &arr,vector<vector<int>>&dp){
+    int solve(int i,int j,vector<int>&arr,vector<vector<int>>&dp){
         
-        if( i>= j){
+        if( i>=j){
             return 0;
-            
         }
         
         if(dp[i][j]!=-1){
@@ -25,46 +15,20 @@ class Solution {
         
         for(int k=i;k<j;k++){
             
-            int oper=arr[i-1]*arr[k]*arr[j]+solve(i,k,arr,dp)+solve(k+1,j,arr,dp);
+            int oper = arr[i-1]*arr[k]*arr[j] + solve(i,k,arr,dp) + solve(k+1,j,arr,dp);
             
-            mini=min(mini,oper);
+            mini = min(mini,oper);
         }
         
-        return dp[i][j]=mini;
+        return dp[i][j] = mini;
     }
     
     int matrixMultiplication(vector<int> &arr) {
         // code here
-        vector<vector<int>>dp(arr.size()+1,vector<int>(arr.size()+1,-1));
         
-        return solve(1,arr.size()-1,arr,dp);
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        return solve(1,n-1,arr,dp);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.matrixMultiplication(a) << endl << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
